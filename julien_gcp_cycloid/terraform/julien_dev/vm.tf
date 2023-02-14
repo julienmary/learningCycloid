c9ea5dbf89c3bafd16d0fedce0e54832dcc4f138
Julien MARY <julien.mary@accenture.com>
2023-02-14 09:18:27 +0000

    Create Config Repository by JulienLearning

--- julien_gcp_cycloid/terraform/julien_dev/vm.tf
+++ julien_gcp_cycloid/terraform/julien_dev/vm.tf
@@ -49,7 +49,7 @@ module "vm" {
   instance_extra_labels = {
   }
 
-  instance_name = "Learning-Cycloid"
+  instance_name = "learning-cycloid"
   instance_tags = [
     "julien-learning-cycloid"
   ]
